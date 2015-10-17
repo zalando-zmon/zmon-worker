0465890004c6e06dd80241454cbd324481e015b3
Jan-M <janm81@gmail.com>
2015-10-17 13:30:34 +0200

    Move code to main()

--- app.py
+++ app.py
@@ -3,29 +3,34 @@ import subprocess
 
 # WRITE ENV VARS TO CONFIG FILE
 
-env_keys = set(["WORKER_NOTIFICATION_SMS_APIKEY",
-               "WORKER_MYSQL_USER",
-               "WORKER_MYSQL_PASSWORD",
-               "SCALYR_READ_KEY",
-               "WORKER_POSTGRESQL_USER",
-               "WORKER_POSTGRESQL_PASSWORD",
-               "WORKER_ACCOUNT",
-               "WORKER_TEAM"])
+def main():
 
-conf = open('/app/web.conf', 'ab')
+    env_keys = set(["WORKER_NOTIFICATION_SMS_APIKEY",
+                   "WORKER_MYSQL_USER",
+                   "WORKER_MYSQL_PASSWORD",
+                   "SCALYR_READ_KEY",
+                   "WORKER_POSTGRESQL_USER",
+                   "WORKER_POSTGRESQL_PASSWORD",
+                   "WORKER_ACCOUNT",
+                   "WORKER_TEAM"])
 
-for k in env_keys:
-    v = os.environ.get(k, None)
-    if v is not None:
-        conf.write(k.replace("WORKER_", "").replace("_",".").lower()+" = " + v + "\n")
+    conf = open('/app/web.conf', 'ab')
 
-redis_keys = set(['broker','backend','redis_servers'])
-redis_host = os.environ.get("WORKER_REDIS_HOST", 'localhost')
-redis_port = os.environ.get("WORKER_REDIS_PORT", '6379')
+    for k in env_keys:
+        v = os.environ.get(k, None)
+        if v is not None:
+            conf.write(k.replace("WORKER_", "").replace("_",".").lower()+" = " + v + "\n")
 
-for k in redis_keys:
-    conf.write("{} = 'redis://{}:{}/0'\n".format(k, redis_host, redis_port))
+    redis_keys = set(['broker', 'backend', 'redis_servers'])
+    redis_host = os.environ.get("WORKER_REDIS_HOST", 'localhost')
+    redis_port = os.environ.get("WORKER_REDIS_PORT", '6379')
 
-conf.close()
+    for k in redis_keys:
+        conf.write("{} = 'redis://{}:{}/0'\n".format(k, redis_host, redis_port))
 
-subprocess.check_output(["zmon-worker", "-c", "/app/web.conf"])
+    conf.close()
+
+    subprocess.check_output(["zmon-worker", "-c", "/app/web.conf"])
+
+if __name__ == "__main__":
+    main()
