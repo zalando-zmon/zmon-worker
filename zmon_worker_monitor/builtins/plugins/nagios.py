76194a80358f421e07a92bd79f847ae137e0167c
Tom Wieczorek <tom.wieczorek@zalando.de>
2015-12-16 10:29:53 +0100

    Port changes from f6e6bd04076fa856aec434d98b20617eeb640bd4 Author: Hanno Hecker <hanno.hecker@zalando.de> Date:   Fri Feb 6 13:51:52 2015 +0100
        add check_dbus check

--- zmon_worker_monitor/builtins/plugins/nagios.py
+++ zmon_worker_monitor/builtins/plugins/nagios.py
@@ -155,6 +155,7 @@ class NagiosWrapper(object):
                           'parameters': {'targethost': 'default', 'port': 443, 'timeout': 10}},
             'check_statistics.pl': {'args': '', 'parser': self._to_dict},
             'check_oracle': {'args': '{user_args}', 'parser': self._to_dict, 'parameters': {'user_args': ''}},
+            'check_dbus': {'args': '', 'parser': self._to_dict_win_text,},
             'check_flocked_file': {'args': '-a {lockfile}', 'parser': self._to_dict_from_text},
             'check_apachestatus_uri': {'args': '-a 16000,10000,48 32000,20000,64 {url}', 'parser': self._to_dict,
                                        'parameters': {'url': 'http://127.0.0.1/server-status?auto'}},
