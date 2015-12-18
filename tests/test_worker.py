04cc7cca22370cdd202acb55a2a8db30cf4a514f
Henning Jacobs <henning@jacobs1.de>
2015-12-18 13:12:16 +0100

    add unit test for missing ctx return

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -7,25 +7,32 @@ import traceback
 from zmon_worker_monitor.web import main
 from mock import MagicMock
 
-def test_check(monkeypatch):
+def build_redis_queue_item(check_command):
+    return {'properties': {'body_encoding': 'nested'},
+            'body': {
+                 'task': 'check_and_notify',
+                 'args': [{'check_id': 123, 'entity': {'id': '77', 'type': 'test'}, 'command': check_command, 'interval': 10}, {}],
+                 'kwargs': {},
+                 'timelimit': [90, 60],
+           }}
+
+def test_check(tmpdir, monkeypatch):
     data = {}
 
     def blpop(key, timeout):
-        print(key, timeout)
-        return key, json.dumps({'properties': {'body_encoding': 'nested'}, 'body': {
-            'task': 'check_and_notify',
-            'args': [{'check_id': 123, 'entity': {'id': '77', 'type': 'test'}, 'command': 'return 1', 'interval': 10}, {}],
-            'kwargs': {},
-            'timelimit': [90, 60],
-            }})
+        assert key == 'zmon:queue:default'
+        return key, json.dumps(build_redis_queue_item('"test-result"'))
 
     def lpush(key, val):
-        print(key, val)
+        data[key] = val
+        with open(str(tmpdir) + 'data.json', 'w') as fd:
+            json.dump(data, fd)
 
     redis = MagicMock()
     redis.blpop = blpop
     redis.lpush = lpush
 
+    # to help debugging: print any worker exceptions
     def exc(*args, **kwargs):
         print(args, kwargs)
         traceback.print_exc()
@@ -34,9 +41,13 @@ def test_check(monkeypatch):
     monkeypatch.setattr('zmon_worker_monitor.workflow.logger.exception', exc)
 
     proc = main(['--no-rpc'])
-    # TODO: test check
+    # make sure the worker processes get enough time to execute our check
     time.sleep(0.1)
     proc.proc_control.terminate_all_processes()
+
+    with open(str(tmpdir) + 'data.json') as fd:
+        data = json.load(fd)
+    assert '"value": "test-result"' in data['zmon:checks:123:77']
 
 
 
