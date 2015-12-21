f41b839f1543888935c230aa767c7199a6f0a2df
Henning Jacobs <henning@jacobs1.de>
2015-12-21 10:41:50 +0100

    remove unused code

--- zmon_worker_monitor/emu_kombu.py
+++ zmon_worker_monitor/emu_kombu.py
@@ -3,33 +3,11 @@
 
 """
 This module is intended as a quick and dirty drop in replacement of kombu module, covering only
-the functionality we use in zmon. In essence we are trying to cover these use cases:
-    from kombu.connection import Connection
-    try:
-        c = Connection(config['backend'])
-    except KeyError:
-        raise Exception('"backend" property missing in config object')
-
-    r_conn = redis.StrictRedis(host=c.hostname, port=c.port, db=c.virtual_host)
-And:
-    from kombu import Queue
-    app.conf.update(CELERY_QUEUES=(Queue('zmon:queue:default',routing_key='default'),
-                    Queue('zmon:queue:secure', routing_key='secure'), Queue('zmon:queue:snmp', routing_key='snmp')))
+the functionality we use in zmon.
 """
 
 import re
 from collections import namedtuple
-
-
-class Queue(object):
-
-    '''
-    Used to emulate kombu Queue
-    '''
-
-    def __init__(self, queue, routing_key=None):
-        self.queue = queue
-        self.routing_key = routing_key
 
 
 def parse_redis_conn(conn_str):
