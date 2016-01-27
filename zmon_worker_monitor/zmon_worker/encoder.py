d720f8286c8386908890235d9ee804ace7ffcc8e
hjacobs <henning@jacobs1.de>
2016-01-27 21:55:04 +0100

    add trivial encoder tests

--- zmon_worker_monitor/zmon_worker/encoder.py
+++ zmon_worker_monitor/zmon_worker/encoder.py
@@ -11,6 +11,18 @@ from decimal import Decimal
 class JsonDataEncoder(json.JSONEncoder):
 
     def default(self, o):
+        '''
+        >>> JsonDataEncoder().encode(datetime.datetime.now())[:3]
+        '"20'
+        >>> JsonDataEncoder().encode(Decimal('3.14'))
+        '3.14'
+        >>> JsonDataEncoder().encode(set([1, 2]))
+        '[1, 2]'
+        >>> JsonDataEncoder().encode(numpy.nan)
+        'null'
+        >>> JsonDataEncoder().encode(numpy.Infinity)
+        '"Infinity"'
+        '''
         if isinstance(o, (datetime.datetime, datetime.date, datetime.time)):
             return o.isoformat()
         elif isinstance(o, Decimal):
@@ -25,14 +37,3 @@ class JsonDataEncoder(json.JSONEncoder):
     def iterencode(self, o, _one_shot=False):
         for chunk in super(JsonDataEncoder, self).iterencode(o, _one_shot=_one_shot):
             yield {'NaN': 'null', 'Infinity': '"Infinity"', '-Infinity': '"-Infinity"'}.get(chunk, chunk)
-
-
-if __name__ == '__main__':
-
-    the_encoder = JsonDataEncoder()
-
-    print repr(the_encoder.encode(numpy.nan))
-    print repr(the_encoder.encode(numpy.Infinity))
-    print repr(the_encoder.encode(-numpy.Infinity))
-    print repr(the_encoder.encode([numpy.nan, numpy.Infinity, -numpy.Infinity]))
-    print repr(the_encoder.encode({'NaN': numpy.nan, 'Infinity': numpy.Infinity, '-Infinity': -numpy.Infinity}))
