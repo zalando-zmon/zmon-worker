771dded14eb819f54c743f5517a5f27ba30c1a81
lmineiro <luis.mineiro@zalando.de>
2016-05-09 14:49:36 +0200

    Keep calling list_metrics while there is a paging token

--- tests/test_cloudwatch.py
+++ tests/test_cloudwatch.py
@@ -57,16 +57,17 @@ def test_cloudwatch_query_one(monkeypatch):
     monkeypatch.setattr('boto3.client', lambda x, region_name: client)
     cloudwatch = CloudwatchWrapper()
     start = datetime.datetime.now()
-    end = start # makes no sense, but works for our test
-    elb_data = cloudwatch.query_one({'LoadBalancerName': 'pierone-1'}, 'Latency', 'Average', 'AWS/ELB', start=start, end=end)
+    end = start  # makes no sense, but works for our test
+    elb_data = cloudwatch.query_one({'LoadBalancerName': 'pierone-1'}, 'Latency', 'Average', 'AWS/ELB', start=start,
+                                    end=end)
     assert 111.25 == elb_data
     assert not client.list_metrics.called
     client.get_metric_statistics.assert_called_with(Namespace='AWS/ELB', MetricName='Latency',
-            Dimensions=[{'Name': 'LoadBalancerName', 'Value': 'pierone-1'}],
-            StartTime=start,
-            EndTime=end,
-            Period=60,
-            Statistics=['Average'])
+                                                    Dimensions=[{'Name': 'LoadBalancerName', 'Value': 'pierone-1'}],
+                                                    StartTime=start,
+                                                    EndTime=end,
+                                                    Period=60,
+                                                    Statistics=['Average'])
 
 
 def test_cloudwatch_query_one_multiple_statistics(monkeypatch):
@@ -82,16 +83,16 @@ def test_cloudwatch_query_one_multiple_statistics(monkeypatch):
     monkeypatch.setattr('boto3.client', lambda x, region_name: client)
     cloudwatch = CloudwatchWrapper()
     start = datetime.datetime.now()
-    end = start # makes no sense, but works for our test
+    end = start  # makes no sense, but works for our test
     elb_data = cloudwatch.query_one({'LoadBalancerName': 'pierone-1'}, 'Latency', None, 'AWS/ELB', start=start, end=end)
     assert {'Average': 111.25, 'Minimum': 1} == elb_data
     assert not client.list_metrics.called
     client.get_metric_statistics.assert_called_with(Namespace='AWS/ELB', MetricName='Latency',
-            Dimensions=[{'Name': 'LoadBalancerName', 'Value': 'pierone-1'}],
-            StartTime=start,
-            EndTime=end,
-            Period=60,
-            Statistics=['Sum', 'Average', 'Maximum', 'SampleCount', 'Minimum'])
+                                                    Dimensions=[{'Name': 'LoadBalancerName', 'Value': 'pierone-1'}],
+                                                    StartTime=start,
+                                                    EndTime=end,
+                                                    Period=60,
+                                                    Statistics=['Sum', 'Average', 'Maximum', 'SampleCount', 'Minimum'])
 
 
 def test_cloudwatch_query_one_no_result(monkeypatch):
@@ -102,6 +103,55 @@ def test_cloudwatch_query_one_no_result(monkeypatch):
     monkeypatch.setattr('requests.get', get)
     monkeypatch.setattr('boto3.client', lambda x, region_name: client)
     cloudwatch = CloudwatchWrapper()
-    elb_data = cloudwatch.query_one({'AvailabilityZone': 'NOT_SET', 'LoadBalancerName': 'pierone-1'}, 'Latency', 'Average', 'AWS/ELB')
+    elb_data = cloudwatch.query_one({'AvailabilityZone': 'NOT_SET', 'LoadBalancerName': 'pierone-1'}, 'Latency',
+                                    'Average', 'AWS/ELB')
     assert elb_data is None
     assert not client.list_metrics.called
+
+
+def test_cloudwatch_paging(monkeypatch):
+    client = MagicMock()
+
+    call_count = {'list_metrics': 0, 'get_metric_statistics': 0}
+
+    def my_list_metrics(*args, **kwargs):
+        call_count['list_metrics'] += 1
+        if 'NextToken' in kwargs:
+            return {'Metrics': [{'MetricName': 'NetworkOut',
+                                 'Namespace': 'AWS/EC2',
+                                 'Dimensions': [
+                                     {'Name': 'AutoScalingGroupName', 'Value': 'tailor-1'}]}]}
+        else:
+            return {'Metrics': [{'MetricName': 'NetworkOut',
+                                 'Namespace': 'AWS/EC2',
+                                 'Dimensions': [
+                                     {'Value': 'i-123456789', 'Name': 'InstanceId'}]},
+                                {'MetricName': 'NetworkOut',
+                                 'Namespace': 'AWS/EC2',
+                                 'Dimensions': [
+                                     {'Value': 'i-987654321', 'Name': 'InstanceId'}]},
+                                {'MetricName': 'NetworkOut',
+                                 'Namespace': 'AWS/EC2',
+                                 'Dimensions': [
+                                     {'Value': 'other-asg-1VSSQK1FPN2A', 'Name': 'AutoScalingGroupName'}]}
+                                ],
+                    'NextToken': 'dummyToken'}
+
+    def my_get_metric_statistics(*args, **kwargs):
+        call_count['get_metric_statistics'] += 1
+        if kwargs['Dimensions'][0]['Name'] == 'AutoScalingGroupName' and kwargs['Dimensions'][0]['Value'] == 'tailor-1':
+            return {'Datapoints': [{'Timestamp': datetime.datetime.now(), 'Average': 42.0}]}
+
+    client.list_metrics = my_list_metrics
+    client.get_metric_statistics = my_get_metric_statistics
+
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    cloudwatch = CloudwatchWrapper(region='dummy-region')
+    data = cloudwatch.query({'AvailabilityZone': 'NOT_SET', 'AutoScalingGroupName': 'tailor-*'},
+                            'NetworkOut', 'Average',
+                            namespace='AWS/EC2')
+
+    assert call_count['list_metrics'] is 2
+    assert call_count['get_metric_statistics'] is 1
+
+    assert {'NetworkOut': 42.0, 'dimensions': {'AutoScalingGroupName': {'tailor-1': 42.0}}} == data
