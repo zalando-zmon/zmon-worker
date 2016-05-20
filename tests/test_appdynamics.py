b65b9ae3100614f91cb49f9b43174798351772be
Mohab Usama <mohab.abdelhameed@zalando.de>
2016-05-20 11:59:39 +0200

    Implement AppDynamics plugin
    + Healthrule violations check
    + Filter by severity

---
+++ tests/test_appdynamics.py
@@ -0,0 +1,172 @@
+import pytest
+import requests
+
+from mock import MagicMock
+
+from zmon_worker_monitor.zmon_worker.errors import HttpError
+from zmon_worker_monitor.zmon_worker.common.http import get_user_agent
+
+from zmon_worker_monitor.builtins.plugins.appdynamics import CRITICAL, WARNING
+from zmon_worker_monitor.builtins.plugins.appdynamics import BEFORE_TIME, BEFORE_NOW, AFTER_TIME, BETWEEN_TIMES
+from zmon_worker_monitor.builtins.plugins.appdynamics import AppdynamicsWrapper
+
+
+USER = 'user'
+PASS = 'pass'
+
+
+def resp_mock(res, failure=False):
+    resp = MagicMock()
+    resp.ok = True if not failure else False
+    resp.json.return_value = res
+
+    return resp
+
+
+def requests_mock(resp, failure=None):
+    req = MagicMock()
+
+    if failure is not None:
+        req.side_effect = failure
+    else:
+        req.return_value = resp
+
+    return req
+
+
+def kwargs_to_params(kwargs):
+    return {k.replace('_', '-'): v for k, v in kwargs.items()}
+
+
+@pytest.fixture(params=[
+    (
+        {'time_range_type': BEFORE_NOW, 'duration_in_mins': 5},  # input
+        [{'name': 'incident', 'severity': 'CRITICAL'}, {'name': 'incident', 'severity': 'WARNING'}]  # output
+    ),
+    (
+        {'time_range_type': BEFORE_TIME, 'duration_in_mins': 5, 'end_time': 112233},
+        [{'name': 'incident', 'severity': 'CRITICAL'}, {'name': 'incident', 'severity': 'WARNING'}]
+    ),
+    (
+        {'time_range_type': AFTER_TIME, 'duration_in_mins': 5, 'start_time': 112233},
+        [{'name': 'incident', 'severity': 'CRITICAL'}, {'name': 'incident', 'severity': 'WARNING'}]
+    ),
+    (
+        {'time_range_type': BETWEEN_TIMES, 'end_time': 112233, 'start_time': 112233},
+        [{'name': 'incident', 'severity': 'CRITICAL'}, {'name': 'incident', 'severity': 'WARNING'}]
+    )
+])
+def fx_violations(request):
+    return request.param
+
+
+@pytest.fixture(params=[CRITICAL, WARNING])
+def fx_severity(request):
+    return request.param
+
+
+@pytest.fixture(params=[
+    (requests.ConnectionError, HttpError),  # (req exception, raised exception)
+    (requests.Timeout, HttpError),
+    (RuntimeError, RuntimeError)
+])
+def fx_exception(request):
+    return request.param
+
+
+@pytest.fixture(params=[
+    {'time_range_type': BEFORE_NOW},
+    {'time_range_type': BEFORE_TIME, 'duration_in_mins': 5},
+    {'time_range_type': BEFORE_TIME, 'end_time': 112233},
+    {'time_range_type': AFTER_TIME, 'duration_in_mins': 5},
+    {'time_range_type': AFTER_TIME, 'start_time': 112233},
+    {'time_range_type': BETWEEN_TIMES, 'end_time': 112233},
+    {'time_range_type': BETWEEN_TIMES, 'start_time': 112233},
+    {'time_range_type': BETWEEN_TIMES, 'duration_in_mins': 5},
+    {'time_range_type': BEFORE_NOW, 'duration_in_mins': 5, 'severity': 'WRONG'},
+])
+def fx_invalid_kwargs(request):
+    return request.param
+
+
+def test_appdynamics_healthrule_violations(monkeypatch, fx_violations):
+    kwargs, violations = fx_violations
+
+    resp = resp_mock(violations)
+    get = requests_mock(resp)
+
+    monkeypatch.setattr('requests.Session.get', get)
+
+    url = 'https://appdynamics'
+    application = 'App 1'
+
+    cli = AppdynamicsWrapper(url, username=USER, password=PASS)
+
+    res = cli.healthrule_violations(application, **kwargs)
+
+    assert violations == res
+    assert (USER, PASS) == cli.session.auth
+    assert get_user_agent() == cli.session.headers['User-Agent']
+    assert 'json' == cli.session.params['output']
+
+    params = kwargs_to_params(kwargs)
+
+    get.assert_called_with(cli.healthrule_violations_url(application), params=params)
+
+
+def test_appdynamics_healthrule_violations_severity(monkeypatch, fx_violations, fx_severity):
+    print fx_violations, fx_severity
+    kwargs, violations = fx_violations
+
+    # URL params
+    params = kwargs_to_params(kwargs)
+
+    resp = resp_mock(violations)
+    get = requests_mock(resp)
+
+    monkeypatch.setattr('requests.Session.get', get)
+
+    url = 'https://appdynamics'
+    application = 'App 1'
+
+    cli = AppdynamicsWrapper(url, username=USER, password=PASS)
+
+    res = cli.healthrule_violations(application, severity=fx_severity, **kwargs)
+
+    assert [v for v in violations if v['severity'] == fx_severity] == res
+    assert (USER, PASS) == cli.session.auth
+    assert get_user_agent() == cli.session.headers['User-Agent']
+    assert 'json' == cli.session.params['output']
+
+    get.assert_called_with(cli.healthrule_violations_url(application), params=params)
+
+
+def test_appdynamics_healthrule_violations_kwargs_error(monkeypatch, fx_invalid_kwargs):
+    url = 'https://appdynamics'
+    application = 'App 1'
+
+    cli = AppdynamicsWrapper(url, username=USER, password=PASS)
+    with pytest.raises(Exception):
+        cli.healthrule_violations(application, **fx_invalid_kwargs)
+
+
+def test_appdynamics_healthrule_violations_errors(monkeypatch, fx_exception):
+    ex, raised = fx_exception
+
+    resp = resp_mock(None, failure=True)
+    get = requests_mock(resp, failure=ex)
+
+    monkeypatch.setattr('requests.Session.get', get)
+
+    url = 'https://appdynamics'
+    application = 'App 1'
+
+    cli = AppdynamicsWrapper(url, username=USER, password=PASS)
+
+    with pytest.raises(raised):
+        cli.healthrule_violations(application, time_range_type=BEFORE_NOW, duration_in_mins=5)
+
+
+def test_appdynamics_healthrule_violations_no_creds():
+    with pytest.raises(Exception):
+        AppdynamicsWrapper('https://appdynamics')
---
+++ zmon_worker_monitor/builtins/plugins/appdynamics.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import logging
+
+import requests
+
+from zmon_worker_monitor.zmon_worker.errors import HttpError
+from zmon_worker_monitor.zmon_worker.common.http import get_user_agent
+
+from zmon_worker_monitor.adapters.ifunctionfactory_plugin import IFunctionFactoryPlugin, propartial
+
+logger = logging.getLogger('zmon-worker.appdynamics-function')
+
+
+BEFORE_NOW = 'BEFORE_NOW'
+BEFORE_TIME = 'BEFORE_TIME'
+AFTER_TIME = 'AFTER_TIME'
+BETWEEN_TIMES = 'BETWEEN_TIMES'
+
+CRITICAL = 'CRITICAL'
+WARNING = 'WARNING'
+
+TIME_RANGE_TYPES = (
+    BEFORE_NOW,
+    BEFORE_TIME,
+    AFTER_TIME,
+    BETWEEN_TIMES,
+)
+
+SEVERITIES = (
+    WARNING,
+    CRITICAL,
+)
+
+
+class AppdynamicsFactory(IFunctionFactoryPlugin):
+    def __init__(self):
+        super(AppdynamicsFactory, self).__init__()
+        # Fields should be fetched from config
+        self._user = None
+        self._pass = None
+
+    def configure(self, conf):
+        """
+        Called after plugin is loaded to pass the [configuration] section in their plugin info file
+        :param conf: configuration dictionary
+        """
+        self._user = conf['user']
+        self._pass = conf['pass']
+
+    def create(self, factory_ctx):
+        """
+        Automatically called to create the check function's object
+        :param factory_ctx: (dict) names available for Function instantiation
+        :return: an object that implements a check function
+        """
+        return propartial(AppdynamicsWrapper, username=self._user, password=self._pass)
+
+
+class AppdynamicsWrapper(object):
+    def __init__(self, url, username=None, password=None):
+        self.url = url
+        self.timeout = 3
+
+        self._username = username
+        self._password = password
+
+        if not username or not password:
+            raise Exception('AppDynamics requires "username" and "password".')
+
+        self._session = requests.Session()
+
+        self._session.auth = (username, password)
+        self._session.headers.update({'User-Agent': get_user_agent()})
+        self._session.params = {'output': 'json'}
+        self._session.timeout = 3
+
+    @property
+    def session(self):
+        return self._session
+
+    def healthrule_violations_url(self, application):
+        return os.path.join(self.url, application, 'problems', 'healthrule-violations')
+
+    def healthrule_violations(self, application, time_range_type='', duration_in_mins=None, start_time=None,
+                              end_time=None, severity=None):
+        """
+        Return Healthrule violations for AppDynamics application.
+
+        :param application: Application name or ID
+        :type application: str
+
+        :param time_range_type: Valid time range type.
+        :type time_range_type: str
+
+        :param duration_in_mins: Time duration in mins. Required for BEFORE_NOW, AFTER_TIME, BEFORE_TIME range types.
+        :type duration_in_mins: int
+
+        :param start_time: Start time (in milliseconds) from which the metric data is returned.
+        :type start_time: int
+
+        :param end_time: End time (in milliseconds) until which the metric data is returned.
+        :type end_time: int
+
+        :param severity: Filter results based on severity. Valid values CRITICAL or WARNING.
+        :type severity: str
+
+        :return: List of healthrule violations
+        :rtype: list
+        """
+        try:
+            params = {}
+
+            if time_range_type not in TIME_RANGE_TYPES:
+                raise Exception('Invalid type! Allowed types are: {}'.format(','.join(TIME_RANGE_TYPES)))
+
+            if severity is not None and severity not in SEVERITIES:
+                raise Exception('Invalid severity! Allowed values are: {}'.format(','.join(SEVERITIES)))
+
+            # Construct query params
+            if time_range_type in (BEFORE_NOW, AFTER_TIME, BEFORE_TIME):
+                if duration_in_mins is None:
+                    raise Exception('Required "duration_in_mins" arg is missing!')
+                params['duration-in-mins'] = duration_in_mins
+
+            if time_range_type in (AFTER_TIME, BETWEEN_TIMES):
+                if start_time is None:
+                    raise Exception('Required "start_time" arg is missing!')
+                params['start-time'] = start_time
+
+            if time_range_type in (BEFORE_TIME, BETWEEN_TIMES):
+                if end_time is None:
+                    raise Exception('Required "end_time" arg is missing!')
+                params['end-time'] = end_time
+
+            params['time-range-type'] = time_range_type
+
+            resp = self.session.get(self.healthrule_violations_url(application), params=params)
+
+            resp.raise_for_status()
+
+            json_resp = resp.json()
+
+            if severity:
+                # we need some filtering!
+                return [e for e in json_resp if e['severity'] == severity]
+
+            return json_resp
+        except requests.Timeout:
+            raise HttpError('timeout', self.url), None, sys.exc_info()[2]
+        except requests.ConnectionError:
+            raise HttpError('connection failed', self.url), None, sys.exc_info()[2]
+        except:
+            logger.exception('AppDynamics request failed')
+            raise
