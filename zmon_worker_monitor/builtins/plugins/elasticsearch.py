e65f33e463b343a1eb5b3fd9b028cf4ff5c034ac
Jan M <janm81@gmail.com>
2016-06-10 11:03:10 +0200

    Add second count function for querying elastic search

--- zmon_worker_monitor/builtins/plugins/elasticsearch.py
+++ zmon_worker_monitor/builtins/plugins/elasticsearch.py
@@ -23,11 +23,11 @@ tokens.configure()
 tokens.manage('uid', ['uid'])
 tokens.start()
 
-
 DEFAULT_SIZE = 10
 MAX_SIZE = 1000
 MAX_INDICES = 10
-
+TYPE_SEARCH = '_search'
+TYPE_COUNT = '_count'
 
 class ElasticsearchFactory(IFunctionFactoryPlugin):
     def __init__(self):
@@ -61,7 +61,14 @@ class ElasticsearchWrapper(object):
         self.oauth2 = oauth2
         self._headers = {'User-Agent': get_user_agent()}
 
+
+    def count(self, indices=None, q='', body=None, source=True, size=DEFAULT_SIZE):
+        return __query(self, TYPE_COUNT, indices, q, body, source, size)
+
     def search(self, indices=None, q='', body=None, source=True, size=DEFAULT_SIZE):
+        return __query(self, TYPE_SEARCH, indices, q, body, source, size)
+
+    def __query(self, query_type, indices, q, body, source, size):
         """
         Search ES cluster using URI search. If ``body`` is None then GET request will be used.
 
@@ -128,7 +135,7 @@ class ElasticsearchWrapper(object):
         if type(indices) is list:
             indices_str = ','.join(indices)
 
-        url = os.path.join(self.url, indices_str, '_search')
+        url = os.path.join(self.url, indices_str, query_type)
 
         if body is None:
             return self.__request(url, params={'q': q, 'size': size, '_source': str(source).lower()})
