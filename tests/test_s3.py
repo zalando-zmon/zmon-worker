13b6c6c1a852a7e8c7f7a9eb5765d3c78f65f6dc
Lorenz Bateman <lorenz.bateman@zalando.de>
2016-12-06 11:36:39 +0100

    Review comments
    Add metadata fetching method to the S3Bucket with .size() and .exists()
    methods on it.
    
    S3BucketWrapperFactory renamed to S3BucketFactory
    
    S3Object remains S3Object since that is what it is called in the S3
    docs.

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,9 +1,72 @@
+from botocore.exceptions import ClientError
+
 from zmon_worker_monitor.builtins.plugins.s3 import S3Bucket
 
 from mock import MagicMock, DEFAULT, ANY
 
 
-def test_raw_object_should_be_found_and_returned(monkeypatch):
+def test_metadata_on_existing_object(monkeypatch):
+
+    client = MagicMock()
+
+    def writer_side_effect(*args, **kwargs):
+        return {'ContentLength': 75}
+    client.head_object.side_effect = writer_side_effect
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'eu-central-1'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    s3_bucket = S3Bucket()
+
+    meta_data = s3_bucket.get_object_metadata('bucket', 'key')
+
+    assert meta_data is not None
+    assert meta_data.exists() is True
+    assert meta_data.size() is 75
+
+
+def test_metadata_on_non_existent_object(monkeypatch):
+
+    client = MagicMock()
+
+    def writer_side_effect(*args, **kwargs):
+        raise ClientError({'Error': {}}, 'some operation')
+    client.head_object.side_effect = writer_side_effect
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'eu-central-1'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    s3_bucket = S3Bucket()
+
+    meta_data = s3_bucket.get_object_metadata('bucket', 'key')
+
+    assert meta_data is not None
+    assert meta_data.exists() is False
+    assert meta_data.size() is -1
+
+
+def test_object_should_not_be_found_and_text_not_returned(monkeypatch):
+    client = MagicMock()
+
+    def writer_side_effect(*args, **kwargs):
+        raise ClientError({'Error': {}}, 'some operation')
+    client.download_fileobj.side_effect = writer_side_effect
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'eu-central-1'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    s3_bucket = S3Bucket()
+
+    s3_object = s3_bucket.get_object('bucket', 'key')
+    raw_object = s3_object.text()
+
+    client.download_fileobj.assert_called_with('bucket', 'key', ANY)
+    assert raw_object is None
+    assert s3_object.exists() is False
+    assert s3_object.size() is -1
+
+
+def test_object_should_be_found_and_text_returned(monkeypatch):
     client = MagicMock()
 
     def writer_side_effect(*args, **kwargs):
@@ -11,19 +74,22 @@ def test_raw_object_should_be_found_and_returned(monkeypatch):
         return DEFAULT
     client.download_fileobj.side_effect = writer_side_effect
     get = MagicMock()
-    get.return_value.json.return_value = {'region': 'myregion'}
+    get.return_value.json.return_value = {'region': 'eu-central-1'}
     monkeypatch.setattr('requests.get', get)
     monkeypatch.setattr('boto3.client', lambda x, region_name: client)
     s3_bucket = S3Bucket()
 
-    raw_object = s3_bucket.get_object('bucket', 'key').text()
+    s3_object = s3_bucket.get_object('bucket', 'key')
+    raw_object = s3_object.text()
 
     client.download_fileobj.assert_called_with('bucket', 'key', ANY)
     assert raw_object is not None
     assert 'some random content' == raw_object
+    assert s3_object.exists() is True
+    assert s3_object.size() is 19
 
 
-def test_json_object_should_be_found_and_returned(monkeypatch):
+def test_object_should_be_found_and_json_returned(monkeypatch):
     client = MagicMock()
 
     def writer_side_effect(*args, **kwargs):
@@ -31,13 +97,16 @@ def test_json_object_should_be_found_and_returned(monkeypatch):
         return DEFAULT
     client.download_fileobj.side_effect = writer_side_effect
     get = MagicMock()
-    get.return_value.json.return_value = {'region': 'myregion'}
+    get.return_value.json.return_value = {'region': 'eu-central-1'}
     monkeypatch.setattr('requests.get', get)
     monkeypatch.setattr('boto3.client', lambda x, region_name: client)
     s3_bucket = S3Bucket()
 
-    json_object = s3_bucket.get_object('bucket', 'key').json()
+    s3_object = s3_bucket.get_object('bucket', 'key')
+    json_object = s3_object.json()
 
     client.download_fileobj.assert_called_with('bucket', 'key', ANY)
     assert json_object is not None
     assert {'content': 'is here', 'some': 'random'} == json_object
+    assert s3_object.exists() is True
+    assert s3_object.size() is 40
--- zmon_worker_monitor/builtins/plugins/s3.py
+++ zmon_worker_monitor/builtins/plugins/s3.py
@@ -7,14 +7,16 @@ import logging
 import requests
 import cStringIO
 
+from botocore.exceptions import ClientError
+
 from zmon_worker_monitor.adapters.ifunctionfactory_plugin import IFunctionFactoryPlugin, propartial
 
 logging.getLogger('botocore').setLevel(logging.WARN)
 
 
-class S3BucketWrapperFactory(IFunctionFactoryPlugin):
+class S3BucketWrapper(IFunctionFactoryPlugin):
     def __init__(self):
-        super(S3BucketWrapperFactory, self).__init__()
+        super(S3BucketWrapper, self).__init__()
 
     def configure(self, conf):
         return
@@ -39,12 +41,23 @@ class S3Bucket(object):
             region = get_region()
         self.__client = boto3.client('s3', region_name=region)
 
+    def get_object_metadata(self, bucket_name, key):
+        try:
+            response = self.__client.head_object(Bucket=bucket_name, Key=key)
+            return S3ObjectMetadata(response)
+        except ClientError:
+            return S3ObjectMetadata({})
+
     def get_object(self, bucket_name, key):
         data = cStringIO.StringIO()
-        self.__client.download_fileobj(bucket_name, key, data)
-        result = data.getvalue()
-        data.close()
-        return S3Object(result)
+        try:
+            self.__client.download_fileobj(bucket_name, key, data)
+            result = data.getvalue()
+            return S3Object(result)
+        except ClientError:
+            return S3Object(None)
+        finally:
+            data.close()
 
 
 class S3Object(object):
@@ -53,7 +66,34 @@ class S3Object(object):
         self.__key_value = key_value
 
     def json(self):
-        return json.loads(self.__key_value)
+        if self.exists():
+            return json.loads(self.__key_value)
+        else:
+            return None
 
     def text(self):
         return self.__key_value
+
+    def exists(self):
+        return self.__key_value is not None
+
+    def size(self):
+        if self.exists():
+            return len(self.__key_value)
+        else:
+            return -1
+
+
+class S3ObjectMetadata(object):
+
+    def __init__(self, response):
+        self.__response = response
+
+    def exists(self):
+        return len(self.__response) > 0
+
+    def size(self):
+        if self.exists():
+            return self.__response['ContentLength']
+        else:
+            return -1
