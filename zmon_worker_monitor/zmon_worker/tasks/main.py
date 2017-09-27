94193d08368b02ec78e5f4267b964e8c51977330
Travis CI <builds@travis-ci.com>
2017-09-27 08:04:15 +0200

    use ISO timestamps in process title
    for easier parsing and with date instead of just hours/minutes

--- zmon_worker_monitor/zmon_worker/tasks/main.py
+++ zmon_worker_monitor/zmon_worker/tasks/main.py
@@ -123,8 +123,9 @@ normalize_kairos_id = propartial(KAIROS_ID_FORBIDDEN_RE.sub, '_')
 
 
 def setp(check_id, entity, msg):
-    setproctitle.setproctitle('zmon-worker check {} on {} {} {}'.format(check_id, entity, msg,
-                                                                        datetime.now().strftime('%H:%M:%S.%f')))
+    setproctitle.setproctitle(
+        'zmon-worker check {} on {} {} {}'.format(check_id, entity, msg,
+                                                  datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')))
 
 
 def get_kairosdb_value(name, points, tags):
