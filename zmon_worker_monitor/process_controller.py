9dab8df076efdb7173b1931f5c1fa8842be15ba0
Travis CI <builds@travis-ci.com>
2017-11-09 10:52:36 +0100

    variable naming

--- zmon_worker_monitor/process_controller.py
+++ zmon_worker_monitor/process_controller.py
@@ -234,7 +234,7 @@ class ProcessPlus(Process):
         'timedelta': 0,
         'tasks_done': 0,
         'percent_idle': 0,
-        'task_time': 0.0,
+        'task_duration': 0.0,
     }
 
     default_status_interval = 60 * 5  # analyze only the pings received since now - this interval
@@ -424,7 +424,7 @@ class ProcessPlus(Process):
             interval = (tnow - self.stored_pings[0]['timestamp']) if self.stored_pings else 0
 
         agg_data = {'tasks_per_sec': -1, 'tasks_per_min': -1, 'percent_idle': -1, 'interval': interval,
-                    'tasks_done': -1, 'pings_received': -1, 'time_per_task': -1}
+                    'tasks_done': -1, 'pings_received': -1, 'average_task_duration': -1}
 
         pings = [p for p in self.stored_pings if tnow - p['timestamp'] <= interval]
         if pings:
@@ -434,7 +434,7 @@ class ProcessPlus(Process):
             agg_data['percent_idle'] = round(float(sum([p['percent_idle'] for p in pings])) / len(pings), FLOAT_DIGITS)
             agg_data['pings_received'] = len(pings)
             if agg_data['tasks_done']:
-                agg_data['time_per_task'] = sum([p['task_time'] for p in pings]) / float(agg_data['tasks_done'])
+                agg_data['average_task_duration'] = sum([p['task_duration'] for p in pings]) / float(agg_data['tasks_done'])
 
         return agg_data
 
