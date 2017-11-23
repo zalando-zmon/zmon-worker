091cee9dedf59215fb04f3e7a49b0d6830fb9e53
Travis CI <builds@travis-ci.com>
2017-11-23 10:41:00 +0100

    use counters like redis plugin
    same `per_sec` postfix

--- zmon_worker_monitor/builtins/plugins/memcached.py
+++ zmon_worker_monitor/builtins/plugins/memcached.py
@@ -8,9 +8,58 @@ from zmon_worker_monitor.zmon_worker.errors import ConfigurationError
 from zmon_worker_monitor.adapters.ifunctionfactory_plugin import IFunctionFactoryPlugin, propartial
 from zmon_worker_monitor import plugin_manager
 
-STRING_KEYS = frozenset([
-    'version',
-    'libevent',
+COUNTER_KEYS = frozenset([
+    'total_connections',
+    'rejected_connections',
+    'cmd_get',
+    'cmd_set',
+    'cmd_flush',
+    'cmd_touch',
+    'cmd_config_get',
+    'cmd_config_set',
+    'get_hits',
+    'get_misses',
+    'get_expired',
+    'get_flushed',
+    'delete_misses',
+    'delete_hits',
+    'incr_misses',
+    'incr_hits',
+    'decr_misses',
+    'decr_hits',
+    'cas_misses',
+    'cas_hits',
+    'cas_badval',
+    'touch_hits',
+    'touch_misses',
+    'auth_cmds',
+    'auth_errors',
+    'bytes_read',
+    'bytes_written',
+    'conn_yields',
+    'slab_reassign_rescues',
+    'slab_reassign_chunk_rescues',
+    'slab_reassign_evictions_nomem',
+    'slab_reassign_inline_reclaim',
+    'slab_reassign_busy_items',
+    'slab_reassign_running',
+    'slabs_moved',
+    'lru_crawler_starts',
+    'lru_maintainer_juggles',
+    'malloc_fails',
+    'log_worker_dropped',
+    'log_worker_written',
+    'log_watcher_skipped',
+    'log_watcher_sent',
+    'crawler_reclaimed',
+    'evictions',
+    'reclaimed',
+    'crawler_items_checked',
+    'lrutail_reflocked',
+    'moves_to_cold',
+    'moves_to_warm',
+    'moves_within_lru',
+    'direct_reclaims',
 ])
 
 
@@ -33,17 +82,21 @@ class MemcachedFactory(IFunctionFactoryPlugin):
         :param factory_ctx: (dict) names available for Function instantiation
         :return: an object that implements a check function
         """
-        return propartial(MemcachedWrapper, host=factory_ctx['host'])
+        # load plugins dependencies and store them locally for efficiency
+        if not self.counter_factory:
+            self.counter_factory = plugin_manager.get_plugin_obj_by_name('counter', 'Function')
+        return propartial(MemcachedWrapper, counter=self.counter_factory.create(factory_ctx), host=factory_ctx['host'])
 
 
 class MemcachedWrapper(object):
     '''Class to allow only readonly access to underlying redis connection'''
 
-    def __init__(self, host, port=11211, socket_connect_timeout=1):
+    def __init__(self, counter, host, port=11211, socket_connect_timeout=1):
         if not host:
             raise ConfigurationError('Memcached wrapper improperly configured. Valid redis host is required!')
 
         self.__con = Client((host, port))
+        self._counter = counter('')
 
     def __del__(self):
         self.__con.quit()
@@ -52,7 +105,14 @@ class MemcachedWrapper(object):
         return self.__con.get(key)
 
     def stats(self):
-        return self.__con.stats()
+        data = self.__con.stats()
+        ret = {}
+        for key in data:
+            if key in COUNTER_KEYS:
+                ret['{}_per_sec'.format(key.replace('total_', ''))] = \
+                    round(self._counter.key(key).per_second(data.get(key, 0)), 2)
+            else:
+                ret[key] = data[key]
 
 
 if __name__ == '__main__':
@@ -61,9 +121,11 @@ if __name__ == '__main__':
 
     # init plugin manager and collect plugins, as done by Zmon when worker is starting
     plugin_manager.init_plugin_manager()
+    plugin_manager.collect_plugins(load_builtins=True, load_env=True)
 
     factory_ctx = {
             'host': 'localhost',
     }
-    wrapper = MemcachedWrapper(sys.argv[1])
+    counter = plugin_manager.get_plugin_obj_by_name('counter', 'Function').create(factory_ctx)
+    wrapper = MemcachedWrapper(counter, sys.argv[1])
     print json.dumps(wrapper.stats(), indent=4, sort_keys=True)
